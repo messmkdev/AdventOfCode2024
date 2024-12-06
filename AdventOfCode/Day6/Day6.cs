f5f49d404869c70bfcdc448c5100c0b043f8fc84
messmk <messmk.dev@gmail.com>
2024-12-06 14:07:32 +0100

    part 2

--- AdventOfCode/Day6/Day6.cs
+++ AdventOfCode/Day6/Day6.cs
@@ -4,18 +4,50 @@ public class Day6{
             var lines = Utils.ReadInputFileAsListString(6);
             //lines.PrintMatrix();
 
-            PathStringMatrix paths = new PathStringMatrix(lines);
-            while(paths.GuardLoS().Contains('#')){
-                //paths.LogLos();
-                paths.WalkLos();
-                //paths.LogMatrix();
+            
+            PathStringMatrix path = new PathStringMatrix(lines);
+            WalkPath(path);
+
+            var positionVisited = path.PositionVisited();
+            positionVisited.RemoveAt(0);
+
+            List<PathStringMatrix> possiblesVariations = new List<PathStringMatrix>();
+
+            foreach(var posVisited in positionVisited){
+                var copy = new PathStringMatrix(lines);
+                copy[posVisited.Item1][posVisited.Item2] = '#';
+                possiblesVariations.Add(copy);
             }
+            var nbVariations = possiblesVariations.Count();
+            var count = possiblesVariations.Count(p => IsLoop(p));
+
+            Console.WriteLine("----------------");
+            Console.WriteLine($"total guard path length {path.visited.Count()}");
+
+    }
+
+    public void WalkPath(PathStringMatrix paths){
+        while(paths.GuardLoS().Contains('#')){
             //paths.LogLos();
             paths.WalkLos();
             //paths.LogMatrix();
+        }
+        //paths.LogLos();
+        paths.WalkLos();
+        //paths.LogMatrix();
+    }
 
-            Console.WriteLine("----------------");
-            Console.WriteLine($"total guard path length {paths.visited.Count()}");
-
+    public bool IsLoop(PathStringMatrix paths){
+        int iterationMax = 10000;
+        int iteration = 0;
+        while(paths.GuardLoS().Contains('#') && iteration <= iterationMax){
+            //paths.LogLos();
+            paths.WalkLos();
+            //paths.LogMatrix();
+            iteration++;
+        }
+        if(iteration == iterationMax + 1)
+            return true;
+        return false;
     }
 }
--- AdventOfCode/Day6/PathStringMatrix.cs
+++ AdventOfCode/Day6/PathStringMatrix.cs
@@ -1,8 +1,10 @@
-public class PathStringMatrix : StringMatrix{
+public class PathStringMatrix : StringMatrix
+{
 
-    public Tuple<int,int> GuardPos {get;set;}
+    public Tuple<int, int> GuardPos { get; set; }
 
-    public PathStringMatrix(List<string> lines) : base(lines){
+    public PathStringMatrix(List<string> lines) : base(lines)
+    {
         GuardPos = GetPos(IsGuard);
     }
 
@@ -10,38 +12,46 @@ public class PathStringMatrix : StringMatrix{
 
     public int GuardCol => GuardPos.Item2;
 
-    char[] guardChars = new []{'<','>','^','v'};
+    char[] guardChars = new[] { '<', '>', '^', 'v' };
 
-    public bool IsGuard(int r, int c){
-        return guardChars.Contains(GetAt(r,c));
+    public bool IsGuard(int r, int c)
+    {
+        return guardChars.Contains(GetAt(r, c));
     }
 
-    public bool IsGuard(char c){
+    public bool IsGuard(char c)
+    {
         return guardChars.Contains(c);
     }
 
-    public bool IsWall(int r, int c) => IsWall(GetAt(r,c));
+    public bool IsWall(int r, int c) => IsWall(GetAt(r, c));
 
     public bool IsWall(char c) => c == '#';
 
     public void Walk(int r, int c) => this[r][c] = 'X';
 
-    public Tuple<int,int>? GetPos(Func<int,int,bool> loop){
-        for(var i = 0 ; i < this.Count; i++){
-            for(var j = 0 ; j < this[i].Count; j++){
-                if(loop(i,j)){
-                    return new Tuple<int,int>(i,j);
+    public Tuple<int, int>? GetPos(Func<int, int, bool> loop)
+    {
+        for (var i = 0; i < this.Count; i++)
+        {
+            for (var j = 0; j < this[i].Count; j++)
+            {
+                if (loop(i, j))
+                {
+                    return new Tuple<int, int>(i, j);
                 }
             }
         }
         return null;
     }
 
-    public LoS GuardLoS(){
+    public LoS GuardLoS()
+    {
         var direction = GuardDirection();
-        switch(direction){
-            case Direction.UP: return new LoS(direction, this.Select( rows => rows[GuardCol]).TakeWhile(c => !IsGuard(c)).Reverse().ToList());
-            case Direction.DOWN: return new LoS(direction, this.Select( rows => rows[GuardCol]).SkipWhile(c => !IsGuard(c)).ToList()[1..]);
+        switch (direction)
+        {
+            case Direction.UP: return new LoS(direction, this.Select(rows => rows[GuardCol]).TakeWhile(c => !IsGuard(c)).Reverse().ToList());
+            case Direction.DOWN: return new LoS(direction, this.Select(rows => rows[GuardCol]).SkipWhile(c => !IsGuard(c)).ToList()[1..]);
             case Direction.LEFT: return new LoS(direction, this[GuardRow].TakeWhile(c => !IsGuard(c)).Reverse().ToList());
             case Direction.RIGHT: return new LoS(direction, this[GuardRow].SkipWhile(c => !IsGuard(c)).ToList()[1..]);
         }
@@ -50,75 +60,107 @@ public class PathStringMatrix : StringMatrix{
 
     public HashSet<string> visited = new HashSet<string>();
 
-    public void WalkLos(){
-        var los = GuardLoS();
-        bool hasWall = los.Contains('#');
-        if(los.Direction == Direction.UP){
-            var verticalDistanceToWall = Math.Abs(GuardRow - los.TakeWhile(c => !IsWall(c)).Count());
-            for(var i = GuardRow ; i > verticalDistanceToWall; i--){
-                this[i][GuardCol] = 'X';
-                visited.Add((i).ToString()+"|"+GuardCol.ToString());
+    public List<Tuple<int, int>> PositionVisited()
+    {
+        return visited.Select(v =>
+        {
+            var p = v.Split("|");
+            return new Tuple<int, int>(int.Parse(p[0]), int.Parse(p[1]));
+        }).ToList();
+    }
+
+    public void WalkLos()
+    {
+        try
+        {
+            var los = GuardLoS();
+            bool hasWall = los.Contains('#');
+            if (los.Direction == Direction.UP)
+            {
+                var verticalDistanceToWall = Math.Abs(GuardRow - los.TakeWhile(c => !IsWall(c)).Count());
+                for (var i = GuardRow; i > verticalDistanceToWall; i--)
+                {
+                    this[i][GuardCol] = 'X';
+                    visited.Add((i).ToString() + "|" + GuardCol.ToString());
+                }
+                this[verticalDistanceToWall][GuardCol] = NextDirection(los);
+
             }
-            this[verticalDistanceToWall][GuardCol] = NextDirection(los);
-            
-        }
-        if(los.Direction == Direction.DOWN){
-            var verticalDistanceToWall = Math.Abs(GuardRow + los.TakeWhile(c => !IsWall(c)).Count());
-            for(var i = GuardRow ; i < verticalDistanceToWall; i++){
-                this[i][GuardCol] = 'X';
-                visited.Add((i).ToString()+"|"+GuardCol.ToString());
+            if (los.Direction == Direction.DOWN)
+            {
+                var verticalDistanceToWall = Math.Abs(GuardRow + los.TakeWhile(c => !IsWall(c)).Count());
+                for (var i = GuardRow; i < verticalDistanceToWall; i++)
+                {
+                    this[i][GuardCol] = 'X';
+                    visited.Add((i).ToString() + "|" + GuardCol.ToString());
+                }
+                this[verticalDistanceToWall][GuardCol] = NextDirection(los);
             }
-            this[verticalDistanceToWall][GuardCol] = NextDirection(los);
-        }
-        if(los.Direction == Direction.LEFT){
-            var horizontalDistanceToWall = Math.Abs(GuardCol - los.TakeWhile(c => !IsWall(c)).Count());
-            for(var i = GuardCol ; i > horizontalDistanceToWall ; i--){
-                this[GuardRow][i] = 'X';
-                visited.Add(GuardRow.ToString()+"|"+i.ToString());
+            if (los.Direction == Direction.LEFT)
+            {
+                var horizontalDistanceToWall = Math.Abs(GuardCol - los.TakeWhile(c => !IsWall(c)).Count());
+                for (var i = GuardCol; i > horizontalDistanceToWall; i--)
+                {
+                    this[GuardRow][i] = 'X';
+                    visited.Add(GuardRow.ToString() + "|" + i.ToString());
+                }
+                this[GuardRow][horizontalDistanceToWall] = NextDirection(los);
             }
-            this[GuardRow][horizontalDistanceToWall] = NextDirection(los);
-        }
-        if(los.Direction == Direction.RIGHT){
-            var horizontalDistanceToWall = Math.Abs(GuardCol + los.TakeWhile(c => !IsWall(c)).Count());
-            for(var i = GuardCol ; i < horizontalDistanceToWall ; i++){
-                this[GuardRow][i] = 'X';
-                visited.Add(GuardRow.ToString()+"|"+i.ToString());
+            if (los.Direction == Direction.RIGHT)
+            {
+                var horizontalDistanceToWall = Math.Abs(GuardCol + los.TakeWhile(c => !IsWall(c)).Count());
+                for (var i = GuardCol; i < horizontalDistanceToWall; i++)
+                {
+                    this[GuardRow][i] = 'X';
+                    visited.Add(GuardRow.ToString() + "|" + i.ToString());
+                }
+                this[GuardRow][horizontalDistanceToWall] = NextDirection(los);
             }
-            this[GuardRow][horizontalDistanceToWall] = NextDirection(los);
+
+            GuardPos = GetPos(IsGuard);
+            if (!hasWall)
+            {
+                this[GuardRow][GuardCol] = 'X';
+                visited.Add(GuardRow.ToString() + "|" + GuardCol.ToString());
+            }
         }
-        
-        GuardPos = GetPos(IsGuard);
-        if(!hasWall){
-            this[GuardRow][GuardCol] = 'X';
-            visited.Add("final");
+        catch (Exception e)
+        {
+            LogMatrix();
         }
     }
 
-    public char NextDirection(LoS los){
-        if(los.Direction == Direction.UP) return '>';
-        if(los.Direction == Direction.RIGHT) return 'v';
-        if(los.Direction == Direction.DOWN) return '<';
-        if(los.Direction == Direction.LEFT) return '^';
+    public char NextDirection(LoS los)
+    {
+        if (los.Direction == Direction.UP) return '>';
+        if (los.Direction == Direction.RIGHT) return 'v';
+        if (los.Direction == Direction.DOWN) return '<';
+        if (los.Direction == Direction.LEFT) return '^';
         return ' ';
     }
 
-    public void LogLos(){
+    public void LogLos()
+    {
         Console.WriteLine("-------------------------");
         Console.WriteLine("Line of sigth");
-        Console.WriteLine(string.Join("",GuardLoS()));
+        Console.WriteLine(string.Join("", GuardLoS()));
     }
 
-    public void LogMatrix(){
+    public void LogMatrix()
+    {
         Console.WriteLine("-------------------------");
-        this.Select(r => string.Join("",r.ToList())).ToList().PrintMatrix();
+        this.Select(r => string.Join("", r.ToList())).ToList().PrintMatrix();
     }
 
-    public Direction GuardDirection(){
-        return CharToDirection(GetAt(GuardRow,GuardCol));
+    public Direction GuardDirection()
+    {
+        return CharToDirection(GetAt(GuardRow, GuardCol));
     }
 
-    public Direction CharToDirection(char c){
-        switch(c){
+    public Direction CharToDirection(char c)
+    {
+        switch (c)
+        {
             case '^': return Direction.UP;
             case 'v': return Direction.DOWN;
             case '>': return Direction.RIGHT;
@@ -129,17 +171,20 @@ public class PathStringMatrix : StringMatrix{
 
 }
 
-public enum Direction{
+public enum Direction
+{
     UP,
     DOWN,
     LEFT,
     RIGHT
 }
 
-public class LoS : List<char>{
-    public Direction Direction{get;set;}
-     
-    public LoS(Direction direction, List<char> los) : base(los){
+public class LoS : List<char>
+{
+    public Direction Direction { get; set; }
+
+    public LoS(Direction direction, List<char> los) : base(los)
+    {
         Direction = direction;
     }
 }
