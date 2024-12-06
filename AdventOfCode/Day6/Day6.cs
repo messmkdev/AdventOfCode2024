d6ff21293dc915816c617c82445a0099a90898ea
messmk <messmk.dev@gmail.com>
2024-12-06 14:47:27 +0100

    day6 part 2

--- AdventOfCode/Day6/Day6.cs
+++ AdventOfCode/Day6/Day6.cs
@@ -1,33 +1,51 @@
-public class Day6{
+using System.Diagnostics;
 
-    public void Work(){
-            var lines = Utils.ReadInputFileAsListString(6);
-            //lines.PrintMatrix();
+public class Day6
+{
 
-            
-            PathStringMatrix path = new PathStringMatrix(lines);
-            WalkPath(path);
+    public void Work()
+    {
+        var lines = Utils.ReadInputFileAsListString(6);
+        //lines.PrintMatrix();
 
-            var positionVisited = path.PositionVisited();
-            positionVisited.RemoveAt(0);
 
-            List<PathStringMatrix> possiblesVariations = new List<PathStringMatrix>();
+        PathStringMatrix path = new PathStringMatrix(lines);
+        WalkPath(path);
 
-            foreach(var posVisited in positionVisited){
-                var copy = new PathStringMatrix(lines);
-                copy[posVisited.Item1][posVisited.Item2] = '#';
-                possiblesVariations.Add(copy);
+        var positionVisited = path.PositionVisited();
+        positionVisited.RemoveAt(0);
+
+        List<PathStringMatrix> possiblesVariations = new List<PathStringMatrix>();
+
+        foreach (var posVisited in positionVisited)
+        {
+            var copy = new PathStringMatrix(lines);
+            copy[posVisited.Item1][posVisited.Item2] = '#';
+            possiblesVariations.Add(copy);
+        }
+
+        int cpt = 0;
+
+        Console.WriteLine("----------------");       
+        Stopwatch s = Stopwatch.StartNew(); 
+        Parallel.For(0, possiblesVariations.Count(), (i) =>
+        {
+            if (IsLoop(possiblesVariations[i]))
+            {
+                Interlocked.Increment(ref cpt);
             }
-            var nbVariations = possiblesVariations.Count();
-            var count = possiblesVariations.Count(p => IsLoop(p));
+        });
+        Console.WriteLine($"Found {cpt} variations with infinite loop");
 
-            Console.WriteLine("----------------");
-            Console.WriteLine($"total guard path length {path.visited.Count()}");
+        Console.WriteLine("----------------");
+        Console.WriteLine($"total guard path length {path.visited.Count()}");
 
     }
 
-    public void WalkPath(PathStringMatrix paths){
-        while(paths.GuardLoS().Contains('#')){
+    public void WalkPath(PathStringMatrix paths)
+    {
+        while (paths.GuardLoS().Contains('#'))
+        {
             //paths.LogLos();
             paths.WalkLos();
             //paths.LogMatrix();
@@ -37,17 +55,24 @@ public class Day6{
         //paths.LogMatrix();
     }
 
-    public bool IsLoop(PathStringMatrix paths){
-        int iterationMax = 10000;
+    public bool IsLoop(PathStringMatrix paths)
+    {
+        Stopwatch stopwatch = Stopwatch.StartNew();
+        int iterationMax = 20000;
         int iteration = 0;
-        while(paths.GuardLoS().Contains('#') && iteration <= iterationMax){
-            //paths.LogLos();
+        while (paths.GuardLoS().Contains('#') && iteration <= iterationMax)
+        {
             paths.WalkLos();
-            //paths.LogMatrix();
             iteration++;
         }
-        if(iteration == iterationMax + 1)
+        stopwatch.Stop();
+
+        if (iteration == iterationMax + 1)
+        {
+            //Console.WriteLine($"Found Loop in{stopwatch.ElapsedMilliseconds} ms");
             return true;
+        }
+        //Console.WriteLine($"Found Not Loop in {stopwatch.ElapsedMilliseconds} ms");
         return false;
     }
 }
